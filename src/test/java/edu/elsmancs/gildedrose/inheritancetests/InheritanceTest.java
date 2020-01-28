e881d33c3163453dcf12a4c43f15617ab9654e0e
dfleta <gelpiorama@gmail.com>
2020-01-28 21:20:23 +0100

    test(InheritenceTest): Implementar casos test herencia AgedBrie

--- src/test/java/edu/elsmancs/gildedrose/inheritancetests/InheritanceTest.java
+++ src/test/java/edu/elsmancs/gildedrose/inheritancetests/InheritanceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import edu.elsmancs.gildedrose.domain.NormalItem;
+import edu.elsmancs.gildedrose.domain.AgedBrie;
 
 
 public class InheritanceTest {
@@ -40,5 +41,23 @@ public class InheritanceTest {
         normal.updateQuality();
         assertEquals(9, normal.getSell_in(), 0);
         assertEquals(0, normal.getQuality(), 0);
+    }
+
+    @Test
+    public void crearAgedBrie() {
+
+        AgedBrie cheese = new AgedBrie("Aged Brie", 2, 0);
+        assertEquals("Aged Brie", cheese.getName());
+        assertEquals(2, cheese.getSell_in(), 0);
+        assertEquals(0, cheese.getQuality(), 0);
+    }
+
+    @Test
+    public void updateQualitycheeseItem() {
+
+        AgedBrie cheese = new AgedBrie("Aged Brie", 2, 0);
+        cheese.updateQuality();
+        assertEquals(1, cheese.getSell_in(), 0);
+        assertEquals(1, cheese.getQuality(), 0);
     }
 }
