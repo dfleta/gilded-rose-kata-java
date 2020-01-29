81a6dca7fdc3843c82ec1ae93a436bb6f0f816e8
dfleta <gelpiorama@gmail.com>
2020-01-29 19:52:31 +0100

    test(InheritenceTest): Testear acceso clases GildedRose, Sulfuras
    Acceso a clases GildedRose y Sulfuras desde otro paquete

--- src/test/java/edu/elsmancs/gildedrose/inheritancetests/InheritanceTest.java
+++ src/test/java/edu/elsmancs/gildedrose/inheritancetests/InheritanceTest.java
@@ -1,10 +1,15 @@
 package edu.elsmancs.gildedrose.inheritancetests;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Test;
 
 import edu.elsmancs.gildedrose.domain.NormalItem;
 import edu.elsmancs.gildedrose.domain.AgedBrie;
+import edu.elsmancs.gildedrose.domain.Sulfuras;
+import edu.elsmancs.gildedrose.domain.GildedRose;
 
 
 public class InheritanceTest {
@@ -12,6 +17,7 @@ public class InheritanceTest {
     @Test
     public void toStringTest() {
         NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 20);
+        System.out.println("toString() inheritance test");
         System.out.println(normal.toString());
     }
 
@@ -59,5 +65,23 @@ public class InheritanceTest {
         cheese.updateQuality();
         assertEquals(1, cheese.getSell_in(), 0);
         assertEquals(1, cheese.getQuality(), 0);
+    }
+
+    @Test
+    public void gildedRoseAddItem() {
+        GildedRose shop = new GildedRose();
+        NormalItem normal = new NormalItem("+5 Dexterity Vest", 10, 20);
+        shop.addItem(normal);
+        AgedBrie cheese = new AgedBrie("Aged Brie", 2, 0);
+        shop.addItem(cheese);
+        Sulfuras sulfuras = new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80);
+        shop.addItem(sulfuras);
+        assertEquals(3, shop.inventory().size(), 0);
+        // nuevo Java 9 metodo factoria estatico para crear unmodifiable lists List.of()
+        List<NormalItem> items = List.of(normal, cheese, sulfuras);
+        assertArrayEquals(items.toArray(), shop.inventory().toArray());
+
+        System.out.println("GildedRose addItem test:");
+        System.out.println(shop.toString());
     }
 }
