c985081f78f14692aa5f04b0ff5ddebc0d84ed80
dfleta <gelpiorama@gmail.com>
2020-01-29 20:03:23 +0100

    test(AgedBrieTest): Testear quality nunca mayor que 50
    Escribrir casos test para chequear que la calidad de un Item Aged Brie
    y, por tanto, Normal Item nunca es mayor que 50

--- src/test/java/edu/elsmancs/gildedrose/domain/AgedBrieTest.java
+++ src/test/java/edu/elsmancs/gildedrose/domain/AgedBrieTest.java
@@ -20,6 +20,7 @@ public class AgedBrieTest {
     @Test
     public void toStringTest() {
         AgedBrie cheese = new AgedBrie("Aged Brie", 2, 0);
+        System.out.println("toString() Aged Brie test:");
         System.out.println(cheese.toString());
     }
 
@@ -48,5 +49,19 @@ public class AgedBrieTest {
         cheese.updateQuality();
         assertEquals(-2, cheese.getSell_in(), 0);
         assertEquals(2, cheese.getQuality(), 0);
+    }
+
+    @Test
+    public void qualityMax50() {
+
+        AgedBrie brie = new AgedBrie("Aged Brie", -1, 50);
+        brie.updateQuality();
+        assertEquals(-2, brie.getSell_in(), 0);
+        assertEquals(50, brie.getQuality(), 0);
+
+        brie = new AgedBrie("Aged Brie", -1, 49);
+        brie.updateQuality();
+        assertEquals(-2, brie.getSell_in(), 0);
+        assertEquals(50, brie.getQuality(), 0);
     }
 }
