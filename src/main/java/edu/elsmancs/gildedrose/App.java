dd3dffc4ec085e4329b9856b1bfd6bac75917d8a
dfleta <gelpiorama@gmail.com>
2020-01-30 21:01:12 +0100

    feat(App): Escribir script principal de la aplicacion
    Crear la tienda, el inventario, actualizarlo y listarlo

--- src/main/java/edu/elsmancs/gildedrose/App.java
+++ src/main/java/edu/elsmancs/gildedrose/App.java
@@ -1,10 +1,65 @@
 package edu.elsmancs.gildedrose;
 
+import java.util.List;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Bienvenido a Ollivanders!" );
+import edu.elsmancs.gildedrose.domain.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenido a Ollivanders!");
+
+        GildedRose shop = new GildedRose();
+
+        /**
+         * Tedious but necessary, Ollivanders types the 
+         * just arrived items and adds them to the 
+         * inventory.
+         */
+
+        List<NormalItem> items = List.of(new NormalItem("+5 Dexterity Vest", 10, 20),
+                                         new AgedBrie("Aged Brie", 2, 0),
+                                         new NormalItem("Elixir of the Mongoose", 5, 7),
+                                         new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+                                         new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
+                                         new Backstage("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                                         new Backstage("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                                         new Backstage("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+        
+        for (NormalItem item : items) {
+            shop.addItem(item);
+        }
+
+        /**
+         * Ollivander lists the inventory
+         */
+        System.out.println("\t ####  DAY 1 ####");
+        System.out.println(shop.toString());
+
+        /**
+         * One day is gone, inventory updated
+         */
+        shop.updateQuality();
+
+        /**
+         * Ollivander lists the inventory
+         */
+        System.out.println("\t ####  DAY 2 ####");
+        System.out.println(shop.toString());
+
+        /**
+         * New item type Conjured just arrived.
+         * Add it to the inventory: 
+         * polimorphism in action
+         */
+        shop.addItem(new Conjured("Conjured Mana Cake", 3, 6));
+        System.out.println(shop.toString());
+
+        /**
+         * Another day is gone, inventory updated
+         */
+        shop.updateQuality();
+        System.out.println("\t ####  DAY 3 ####");
+        System.out.println(shop.toString());
     }
 }
